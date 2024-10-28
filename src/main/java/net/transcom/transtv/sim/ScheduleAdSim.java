d5beb3a0cc0440b81f16afade4f2fb1149a8934a
emmanuel-jan <emmanueljan80@gmail.com>
2024-10-28 10:14:04 +0300

    adding more matatus

--- src/main/java/net/transcom/transtv/sim/ScheduleAdSim.java
+++ src/main/java/net/transcom/transtv/sim/ScheduleAdSim.java
@@ -33,7 +33,15 @@ public class ScheduleAdSim {
                 new Ads("BACK LASH", "Rina TVC", "20", "KDC 279N", "KDC 279N", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim"),
                 new Ads("BIG CHIEF", "Rina TVC", "20", "KDA 067D", "KDA 067D", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim"),
                 new Ads("BLACK MARKET", "Rina TVC", "20", "KCR 328A", "KCR 328A", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim"),
-                new Ads("VOICE MAIL", "Rina TVC", "20", "KDD 001L", "KDD 001L", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim")
+                new Ads("VOICE MAIL", "Rina TVC", "20", "KDD 001L", "KDD 001L", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim"),
+                new Ads("ELSABROSA", "Rina TVC", "20", "KDB 037E", "KDB 037E", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim"),
+                new Ads("OPPOSITE", "Rina TVC", "20", "KDH 112E", "KDH 112E", "31", "CBD,KARIKOR JUJA PUMWANI, DANDORA", 0, 0, false, true, "sim"),
+                new Ads("QUEENTA", "Rina TVC", "20", "KCW 079R", "KCW 079R", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim"),
+                new Ads("BIONIC", "Rina TVC", "20", "KDK 063K", "KDK 063K", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim"),
+                new Ads("BVB", "Rina TVC", "20", "KDK 216U", "KDK 216U", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim"),
+                new Ads("AMBUSH", "Rina TVC", "20", "KDG 195K", "KDG 195K", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim"),
+                new Ads("ISSACHAR", "Rina TVC", "20", "KDM 117M", "KDM 117M", "31", "JOGOO RD, MOMBASA RD, THIKA RD", 0, 0, false, true, "sim")
+
         );
 
         adsRepository.saveAll(dataList);
