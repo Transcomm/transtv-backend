b5dc56fa4f8c543f6ae3fa0a71894c5da0dc0df4
emmanuel-jan <emmanueljan80@gmail.com>
2024-12-16 12:33:54 +0300

    updating the TVC

--- src/main/java/net/transcom/transtv/sim/ScheduleAdSim.java
+++ src/main/java/net/transcom/transtv/sim/ScheduleAdSim.java
@@ -43,21 +43,21 @@ public class ScheduleAdSim {
                 new Ads("ISSACHAR", "Rina TVC", "20", "KDM 117M", "KDM 117M", "31", "THIKA ROAD", 0, 0, false, true, "sim"),
 
                 // Girl Effect
-                new Ads("FUNKADELICA", "Girl Effect TVC", "22", "KDK 983A", "KDK 983A", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
-                new Ads("STEAMPUNK", "Girl Effect TVC", "22", "KDA 087L", "KDA 087L", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
-                new Ads("DROID", "Girl Effect TVC", "22", "KDN 267J", "KDN 267J", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
-                new Ads("SQUID GAME", "Girl Effect TVC", "22", "KDH 049C", "KDH 049C", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
-                new Ads("B0YKA", "Girl Effect TVC", "22", "KCD 844E", "KCD 844E", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
-                new Ads("WAVE 1", "Girl Effect TVC", "22", "KDH 304C", "KDH 304C", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
-                new Ads("FORTUNE", "Girl Effect TVC", "22", "KDC 279N", "KDC 279N", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
-                new Ads("BUMAYE", "Girl Effect TVC", "22", "KDA 067D", "KDA 067D", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
-                new Ads("G-UNIT", "Girl Effect TVC", "22", "KDD 001L", "KDD 001L", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
-                new Ads("ELSABROSA", "Girl Effect TVC", "22", "KDB 037E", "KDB 037E", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
-                new Ads("QUEENTA", "Girl Effect TVC", "22", "KCW 079R", "KCW 079R", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
-                new Ads("BIONIC", "Girl Effect TVC", "22", "KDK 063K", "KDK 063K", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
-                new Ads("BVB", "Girl Effect TVC", "22", "KDK 216U", "KDK 216U", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
-                new Ads("FROOTY", "Girl Effect TVC", "22", "KDG 195K", "KDG 195K", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
-                new Ads("ISSACHAR", "Girl Effect TVC", "22", "KDM 117M", "KDM 117M", "31", "NGONG ROAD", 0, 0, false, true, "sim")
+                new Ads("FUNKADELICA", "Tuki Episode 2", "22", "KDK 983A", "KDK 983A", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
+                new Ads("STEAMPUNK", "Tuki Episode 2", "22", "KDA 087L", "KDA 087L", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
+                new Ads("DROID", "Tuki Episode 2", "22", "KDN 267J", "KDN 267J", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
+                new Ads("SQUID GAME", "Tuki Episode 2", "22", "KDH 049C", "KDH 049C", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
+                new Ads("B0YKA", "Tuki Episode 2", "22", "KCD 844E", "KCD 844E", "31", "JOGOO ROAD", 0, 0, false, true, "sim"),
+                new Ads("WAVE 1", "Tuki Episode 2", "22", "KDH 304C", "KDH 304C", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
+                new Ads("FORTUNE", "Tuki Episode 2", "22", "KDC 279N", "KDC 279N", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
+                new Ads("BUMAYE", "Tuki Episode 2", "22", "KDA 067D", "KDA 067D", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
+                new Ads("G-UNIT", "Tuki Episode 2", "22", "KDD 001L", "KDD 001L", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
+                new Ads("ELSABROSA", "Tuki Episode 2", "22", "KDB 037E", "KDB 037E", "31", "MOMBASA ROAD", 0, 0, false, true, "sim"),
+                new Ads("QUEENTA", "Tuki Episode 2", "22", "KCW 079R", "KCW 079R", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
+                new Ads("BIONIC", "Tuki Episode 2", "22", "KDK 063K", "KDK 063K", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
+                new Ads("BVB", "Tuki Episode 2", "22", "KDK 216U", "KDK 216U", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
+                new Ads("FROOTY", "Tuki Episode 2", "22", "KDG 195K", "KDG 195K", "31", "NGONG ROAD", 0, 0, false, true, "sim"),
+                new Ads("ISSACHAR", "Tuki Episode 2", "22", "KDM 117M", "KDM 117M", "31", "NGONG ROAD", 0, 0, false, true, "sim")
 
         );
 
