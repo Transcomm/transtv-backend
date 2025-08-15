b34460741dc6005c634e93d40cb51b518027c8d8
transcomm <transcommdev@gmail.com>
2025-08-15 09:49:38 +0300

    updating the response table

--- src/main/java/net/transcom/transtv/entities/SurveyResponse.java
+++ src/main/java/net/transcom/transtv/entities/SurveyResponse.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "tbl_survey_responses")
@@ -16,7 +17,12 @@ public class SurveyResponse {
     private Date submittedAt = new Date();
     private String question1;
     private String question2;
-    private String question3;
+
+    @ElementCollection
+    @CollectionTable(name = "survey_preferences", joinColumns = @JoinColumn(name = "survey_id"))
+    @Column(name = "preference")
+    private List<String> question3;
+
     private String question4;
     private String question5;
 
